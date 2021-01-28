bc66499a098e816bdcaa53e2ab6d4f65c7eb4df8
Chocological <cytusomega@gmail.com>
2021-01-28 10:57:10 -0800

    movement coded

--- 2048.py
+++ 2048.py
@@ -35,10 +35,53 @@ class Game:
                     surface.blit(text, (10, 30))
                     gamearea.blit(surface, (x*100, y*100))
 
+    def move_left(self):
+        for y in range(4):
+            space = 0
+            for x in range(4):
+                if self.gamedata[y][x] == 0:
+                    space += 1
+                    continue
+                else:
+                    if space > 0:
+                        self.gamedata[y][x-space] = self.gamedata[y][x]
+                        self.gamedata[y][x] = 0
+    def move_right(self):
+        for y in range(4):
+            space = 0
+            for x in range(4):
+                x = 3-x
+                if self.gamedata[y][x] == 0:
+                    space += 1
+                    continue
+                else:
+                    if space > 0:
+                        self.gamedata[y][x+space] = self.gamedata[y][x]
+                        self.gamedata[y][x] = 0
     def move_up(self):
         for x in range(4):
+            space = 0
             for y in range(4):
-                if   
+                if self.gamedata[y][x] == 0:
+                    space += 1
+                    continue
+                else:
+                    if space > 0:
+                        self.gamedata[y-space][x] = self.gamedata[y][x]
+                        self.gamedata[y][x] = 0
+    def move_down(self):
+        for x in range(4):
+            space = 0
+            for y in range(4):
+                y = 3-y
+                if self.gamedata[y][x] == 0:
+                    space += 1
+                    continue
+                else:
+                    if space > 0:
+                        self.gamedata[y+space][x] = self.gamedata[y][x]
+                        self.gamedata[y][x] = 0
+                    
 
 
 gameplay = Game()
@@ -47,6 +90,9 @@ gameplay.addtile(3, 1, 120)
 gameplay.addtile(2, 0, 64)
 gameplay.addtile(1, 3, 12)
 gameplay.addtile(0, 1, 8)
+gameplay.addtile(3, 2, 32)
+gameplay.addtile(1, 0, 180)
+gameplay.addtile(3, 0, 2)
 
 '''
 class Tile:
@@ -94,6 +140,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                gameplay.move_left()
+            if event.key == pygame.K_RIGHT:
+                gameplay.move_right()
+            if event.key == pygame.K_UP:
+                gameplay.move_up()
+            if event.key == pygame.K_DOWN:
+                gameplay.move_down()
     pygame.display.update()
     clock.tick(30)
---
+++ README.md
@@ -0,0 +1,6 @@
+# 2048 game
+
+Work in progress game of 2048 using pygame
+
+game field done, tiles done, movement down, no block merging yet
+
